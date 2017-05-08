177dc17a0096e3bc41a20f2a2fdd17fd4bf3269a
Connor O'Day <connoroday@connors-mbp-4.middlebury.edu>
2017-05-08 18:52:40 -0400

    Colors

--- YouLift/YouLift/WorkoutTableViewController.swift
+++ YouLift/YouLift/WorkoutTableViewController.swift
@@ -15,6 +15,23 @@ class WorkoutTableViewController: UIViewController, UITableViewDelegate, UITable
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor(hue: 0.4, saturation: 0.05, brightness: 0.9, alpha: 1.0)
+        
+        self.tableView!.layer.shadowOffset = CGSize(width: 0, height: 0)
+        self.tableView!.layer.shadowColor = UIColor.black.cgColor
+        self.tableView!.layer.shadowRadius = 5
+        self.tableView!.layer.shadowOpacity = 0.3
+        self.tableView!.layer.masksToBounds = false;
+        self.tableView!.clipsToBounds = false;
+        
+        self.customTableView!.layer.shadowOffset = CGSize(width: 0, height: 0)
+        self.customTableView!.layer.shadowColor = UIColor.black.cgColor
+        self.customTableView!.layer.shadowRadius = 5
+        self.customTableView!.layer.shadowOpacity = 0.3
+        self.customTableView!.layer.masksToBounds = false;
+        self.customTableView!.clipsToBounds = false;
+
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -38,6 +55,8 @@ class WorkoutTableViewController: UIViewController, UITableViewDelegate, UITable
     
     private let workouts = WorkoutCollection()
     
+    let tableColor = UIColor(red: 147/255, green: 234/255, blue: 215/255, alpha: 0.5)
+    
 
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -48,6 +67,9 @@ class WorkoutTableViewController: UIViewController, UITableViewDelegate, UITable
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+         UITableViewCell.appearance().backgroundColor = tableColor
+        
         if tableView == self.tableView {
             
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "WorkoutTableViewCell", for: indexPath) as? WorkoutTableViewCell else{
